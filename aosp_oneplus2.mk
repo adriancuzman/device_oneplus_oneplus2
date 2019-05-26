6d1939afcad7644af596e42ba352414b00d2858a
danny2895 <vigneshkrissh95@gmail.com>
2019-05-26 21:48:57 +0530

    Revert "Total rebase"
    This reverts commit 8df0986b86cf4d6868370140ea098258554390c8.

---
+++ aosp_oneplus2.mk
@@ -0,0 +1,43 @@
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_l_mr1.mk)
+
+# Inherit from oneplus2 device
+$(call inherit-product, device/oneplus/oneplus2/device.mk)
+
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_GAPPS_ARCH := arm64
+CUSTOM_BUILD_TYPE := OFFICIAL
+
+# Inherit some common PixelExperience stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+
+PRODUCT_NAME := aosp_oneplus2
+PRODUCT_DEVICE := oneplus2
+PRODUCT_MANUFACTURER := OnePlus
+PRODUCT_BRAND := OnePlus
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="OnePlus2-user 6.0.1 MMB29M 7 dev-keys"
+
+PRODUCT_GMS_CLIENTID_BASE := android-oneplus
+
+BUILD_FINGERPRINT := "OnePlus/OnePlus2/OnePlus2:6.0.1/MMB29M/1447858500:user/release-keys"
+
+TARGET_VENDOR := oneplus
